f44c7889240d7636a4ee94091d4e9e712d3b6166
russrezepov <russr2017@gmail.com>
2020-04-05 13:19:53 -0400

    Initial commit

---
+++ app/src/main/java/com/russrezepov/newsapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.russrezepov.newsapp;
+
+//newsapi.org/docs/endpoints/top-headlines
+//API_KEY="d03e17d8f4ab4b2fabf3a3a7d37e9d52"
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
